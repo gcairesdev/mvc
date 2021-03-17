dfa488e5c4fb6af9cc70e6973687fb3c0167883b
Guilherme Caires <pessoalgcaires@gmail.com>
2021-03-17 19:45:40 -0300

    fix: handled empty URL

--- Application.php
+++ Application.php
@@ -2,22 +2,26 @@
 
 define('INCLUDE_PATH', 'http://localhost/mvc/');
 define('INCLUDE_PATH_FULL', 'http://localhost/mvc/Views/pages/');
+
 class Application
 {
 
     public function run()
     {
-        $url = $_GET['url'];
+        $url = isset($_GET['url']) ? $_GET['url'] : null;
+        
         $urlFirstArg = explode('/', $url)[0];
 
-        $controller = isset($url) ? $urlFirstArg : 'Home';
+        $controller = $url ? $urlFirstArg : 'Home';
         $controller = ucfirst($controller) . 'Controller';
 
-        if (file_exists("Controllers/$controller.php")) {
+        if (file_exists("Controllers/$controller.php"))
+        {
             $controler = new $controller();
-            $controler->run();
-        } else {
-            die("Página não encontrada!");
+            return $controler->run();
         }
+
+        return die('Página não encontrada!');
     }
+
 }
