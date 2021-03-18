ecdf44e62ba35f8ae0708532d067dfcd2ae55ca7
Guilherme Caires <pessoalgcaires@gmail.com>
2021-03-17 21:54:17 -0300

    refactor: start app

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
 
-$autoload = function ($class) {
-
-    if (file_exists('Controllers/' . $class . '.php')) {
+$autoload = function ($class)
+{
+    if (file_exists($class . '.php')) include($class . '.php');
+    
+    if (file_exists('Controllers/' . $class . '.php'))
         include('Controllers/' . $class . '.php');
-    } else if (file_exists($class . '.php')) {
-        include($class . '.php');
-    }
 };
 
 spl_autoload_register($autoload);
