96e3c4ad19acea02ea73058226fa0d740c59b46c
Guilherme Caires <pessoalgcaires@gmail.com>
2021-03-17 22:29:25 -0300

    feat: changed contact success

--- Views/pages/contato-sucesso.php
+++ Views/pages/contato-sucesso.php
@@ -1,19 +1,49 @@
-<div class="alert alert-success alert-dismissible fade show text-center" role="alert">
-    <strong>Olá!</strong> Sua mensagem foi enviada com sucesso!
+<div class="alert alert-success alert-dismissible fade show text-center">
+    <strong>Viva!</strong> Sua mensagem foi enviada com sucesso.
+
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
     </button>
 </div>
-<div class="container">
+
+<div class="container mt-5">
+
+    <div class="alert alert-success" role="alert">
+        <h4 class="alert-heading">Bem vindo!</h4>
+
+        <p>Esta é a tela de contato com sucesso!<p>
+
+        <hr>
+
+        <p>Você pode alterar seu conteúdo em <?= __FILE__ ?></p>
+    </div>
+
     <form method="post">
         <div class="form-group">
-            <input type="text" name="nome" placeholder="Nome.." class="form-control">
+            <input
+                type="text"
+                name="nome"
+                placeholder="Nome"
+                class="form-control"
+            >
         </div>
+
         <div class="form-group">
-            <textarea name="mensagem" placeholder="Sua mensagem..." class="form-control"></textarea>
+            <textarea
+                name="mensagem"
+                placeholder="Mensagem"
+                class="form-control"
+            ></textarea>
         </div>
+
         <div class="form-group">
-            <input type="submit" name="acao" value="Enviar!" class="form-control">
+            <input
+                type="submit"
+                name="acao"
+                value="Enviar!"
+                class="form-control"
+            >
         </div>
     </form>
+
 </div>
