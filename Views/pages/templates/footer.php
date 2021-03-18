00793d56f9c7e1c0b27ef9155b0c01594b7718a6
Guilherme Caires <pessoalgcaires@gmail.com>
2021-03-17 22:27:01 -0300

    feat: added footer

--- Views/pages/templates/footer.php
+++ Views/pages/templates/footer.php
@@ -1,3 +1,8 @@
+    <footer class="alert alert-secondary text-center fixed-bottom mb-0">
+        Made with ❤️ by
+        <a href="https://github.com/gcairesdev/mvc">Guilherme Caires</a>
+    </footer>
+
     <script src="<?= INCLUDE_PATH_FULL ?>js/jquery-3.4.1.slim.min.js"></script>
     <script src="<?= INCLUDE_PATH_FULL ?>js/popper.min.js"></script>
 	<script src="<?= INCLUDE_PATH_FULL ?>js/bootstrap.min.js"></script>
